665977942ab33b25fb449aece18f32939330a9b8
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2024-12-18 22:34:30 -0600

    ChallengeChallenge prompt

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
 'use strict';
 
-//Write a function that removes every lone 9 that is inbetween 7s.
-//"79712312" --> "7712312"
-//"79797"    --> "777"
-
-function sevenAte9(str) {
-  const numberInput = String(str);
-  const array = numberInput.split('');
-
-  let newNumber = '';
-
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === '9' && array[i - 1] === '7' && array[i + 1] === '7') {
-      array.splice(i, 1);
-    } else {
-      array;
-    }
-  }
-
-  console.log(array);
-
-  for (let j = 0; j < array.length; j++) {
-    newNumber = newNumber + array[j];
-  }
-
-  return newNumber;
-}
+// Beaches are filled with sand, water, fish, and sun.
+// Given a string, calculate how many times the words
+// "Sand", "Water", "Fish", and "Sun" appear without
+// overlapping (regardless of the case).
