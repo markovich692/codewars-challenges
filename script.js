8734cdd2b280899148ed4e0af50fbc5b46906b8b
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2024-12-19 23:04:52 -0600

    builds the mean calculator function

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 
 //Find Mean
 //Find the mean (average) of a list of numbers in an array.
+
+const mean = function (array) {
+  let total = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+
+    total = total + array[i];
+  }
+
+  return total / array.length;
+};
+
+console.log(mean([4, 4]));
