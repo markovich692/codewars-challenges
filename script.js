4634f00e229543e4311b31828ad8d6b183ce5d4d
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2024-12-19 23:15:16 -0600

    states the challenge

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 'use strict';
 
-//Find Mean
-//Find the mean (average) of a list of numbers in an array.
-
-const mean = function (array) {
-  let total = 0;
-
-  for (let i = 0; i < array.length; i++) {
-    console.log(array[i]);
-
-    total = total + array[i];
-  }
-
-  return total / array.length;
-};
-
-console.log(mean([4, 4]));
+//Given an integral number, determine if it's a square number:
+//In mathematics, a square number or perfect square is an integer
+//that is the square of an integer; in other words, it is the
+// product of some integer with itself.
