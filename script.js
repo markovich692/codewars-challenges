21b7470c43704b9e124855c1c141671719c705a3
Marc Diallo <marcdiallo@Marcs-MacBook-Pro.local>
2024-12-20 00:29:16 -0600

    writes the function that checks if function is square

--- script.js
+++ script.js
@@ -4,3 +4,11 @@
 //In mathematics, a square number or perfect square is an integer
 //that is the square of an integer; in other words, it is the
 // product of some integer with itself.
+
+const isSquare = function (number) {
+  if (number % Math.sqrt(number) === 0 || number === 0) {
+    return false;
+  } else {
+    return false;
+  }
+};
