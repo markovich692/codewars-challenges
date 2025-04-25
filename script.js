c86af37935e1d0f40d31b078290a012608b91f33
Marc Diallo <mark.devguru@outlook.com>
2025-04-25 14:44:23 -0500

    starts new challenge

--- script.js
+++ script.js
@@ -12,3 +12,7 @@ const isSquare = function (number) {
     return false;
   }
 };
+
+// We need to sum big numbers and we require your help.
+// Write a function that returns the sum of two numbers.
+//  The input numbers are strings and the function must return a string.
