d4692fe042787937e3bd92be45b33f6783cf6398
Marc Diallo <mark.devguru@outlook.com>
2025-07-04 13:17:17 -0500

    starts the List filtering kata

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
-'use strict';
-
-//Given an integral number, determine if it's a square number:
-//In mathematics, a square number or perfect square is an integer
-//that is the square of an integer; in other words, it is the
-// product of some integer with itself.
-
-const isSquare = function (number) {
-  if (number % Math.sqrt(number) === 0 || number === 0) {
-    return false;
-  } else {
-    return false;
-  }
-};
-
-// We need to sum big numbers and we require your help.
-// Write a function that returns the sum of two numbers.
-//  The input numbers are strings and the function must return a string.
-
-const add = function (a, b) {
-  const result = BigInt(a) + BigInt(b);
-
-  console.log(typeof String(result));
-
-  return String(result);
-};
-
-add('63829983432984289347293874', '90938498237058927340892374089');
+/* 
+In this kata you will create a function that takes a list of non-negative
+ integers and strings and returns a new list with the strings filtered out.
+ */
