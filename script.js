9acd45e96aaba8b0efada97b5d8c5291315d9f82
Marc Diallo <mark.devguru@outlook.com>
2025-07-05 08:55:36 -0500

    adds the condition for the message to be displayed accordingly

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ Write a program where Alex can input (n) how many times the hoop goes round and 
 If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
 If he doesn't get 10 hoops, return the string "Keep at it until you get it".
 */
+
+function hoopCount(n) {
+  if (typeof n !== 'number') return;
+
+  return n >= 10
+    ? 'Great, now move on to tricks'
+    : 'Keep at it until you get it';
+}
+
+const test = hoopCount(11);
+
+console.log(test);
