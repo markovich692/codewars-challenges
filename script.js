60ea582c2a3d331186f4cc7dd11e0b2d6ccefa9e
Marc Diallo <mark.devguru@outlook.com>
2025-07-30 21:16:38 -0500

    defines the plus() and two() functions

--- script.js
+++ script.js
@@ -12,8 +12,15 @@ eight(minus(three()));  //  must return 5
 six(dividedBy(two()));  //  must return 3
 */
 
-/*
-function two() {}
+function zero() {}
+function one() {}
+
+function two(operandFirst) {
+  if (!operandFirst) return 2;
+  const res = 2 + Number(operandFirst);
+  return res;
+}
+
 function three() {}
 function four() {}
 function five() {}
@@ -21,19 +28,29 @@ function six() {}
 function seven() {}
 function eight() {}
 function nine() {}
+
+function plus(operator) {
+  const add = '+' + String(operator);
+  return add;
+}
+
 function minus() {}
 function times() {}
 function dividedBy() {}
+
+/*
+function two(operandFirst) {
+  if (!operandFirst) return 2;
+
+  const res = 2 + Number(operandFirst);
+  return res;
+}
+
+function plus(operator) {
+  const add = '+' + String(operator);
+  return add;
+}
+
+const finalResult = two(plus(two()));
+console.log(finalResult);
 */
-
-function two(operand = 2) {
-  return operand;
-}
-
-function plus(operand) {
-  return +operand;
-}
-
-const res = plus(two());
-
-console.log(res);
