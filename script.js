a89f4ea0c3d9a2500f036460ea82226914faaca7
Marc Diallo <mark.devguru@outlook.com>
2025-07-30 19:07:03 -0500

    defines the two() and the plus() function and calls the plus(two()) and stores it as the res variable

--- script.js
+++ script.js
@@ -5,4 +5,35 @@ There must be a function for each of the following mathematical operations: plus
 Each calculation consist of exactly one operation and two numbers
 The most outer function represents the left operand, the most inner function represents the right operand
 Division should be integer division. For example, this should return 2, not 2.666666...:
+
+seven(times(five()));   //  must return 35
+four(plus(nine()));     //  must return 13
+eight(minus(three()));  //  must return 5
+six(dividedBy(two()));  //  must return 3
 */
+
+/*
+function two() {}
+function three() {}
+function four() {}
+function five() {}
+function six() {}
+function seven() {}
+function eight() {}
+function nine() {}
+function minus() {}
+function times() {}
+function dividedBy() {}
+*/
+
+function two(operand = 2) {
+  return operand;
+}
+
+function plus(operand) {
+  return +operand;
+}
+
+const res = plus(two());
+
+console.log(res);
